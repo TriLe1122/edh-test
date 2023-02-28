2be8450bae8be8d7e7a66db408624dcffaa6a297
tri le <letrin93@gmail.com>
2023-02-27 23:00:48 -0700

    call to api

--- edh-test.client/src/pages/HomePage.vue
+++ edh-test.client/src/pages/HomePage.vue
@@ -3,8 +3,22 @@
 </template>
 
 <script>
+import { onMounted } from "vue"
+import { cardsService } from "../services/CardsService.js"
+
 export default {
   setup() {
+    async function getCards() {
+      try {
+          await cardsService.getCards()
+        } catch (error) {
+          console.error('[]',error)
+          Pop.error(error)
+        }
+    }
+    onMounted(() => {
+      getCards()
+    })
     return {}
   }
 }
